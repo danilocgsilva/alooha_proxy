75c77108bb5f74c4fef864424b728966ff0e6109
Danilo Silva <contact@danilocgsilva.me>
2026-08-09 13:02:42 -0300

    Updated configurations to upt the updates

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-    transform: {
-        '^.+\\.(ts|tsx)$': ['ts-jest', {
-            useESM: true,
-        }],
-        '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    preset: "ts-jest",
-    extensionsToTreatAsEsm: ['.ts'],
-    testEnvironment: "node",
-    testMatch: ["**/*.test.ts"],
-    slowTestThreshold: 30,
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1"
-    },
-    transformIgnorePatterns: [
-        'node_modules/(?!(uuid|winston|winston-daily-rotate-file)/)'
-    ],
-    testTimeout: 30000
+  transform: {
+    '^.+\\.(ts|tsx)$': ['@swc/jest'],
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  slowTestThreshold: 30,
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(uuid|winston|winston-daily-rotate-file)/)',
+  ],
+  testTimeout: 30000,
 };
